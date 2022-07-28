d177c281d0824a6403626d8f8c7e086a08782853
Fintan Ryan <fintonius.mcginty@gmail.com>
2022-07-28 08:30:04 +0100

    update script to run single instance of game

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-console.log(getComputerChoice());
-
 function getComputerChoice () { //randomly selects 1 of 3 options
 	const choices = [`rock`, `paper`, `scissors`];
 	return choices[Math.floor(Math.random() * 3)];
 }
 
-// const playerChoice = prompt(`Player, pick your weapon: rock, paper or scissors`, ``);
+function playGame () {
+
+	let compChoice = getComputerChoice();
+	let playerChoice = prompt(`Player, pick your weapon: rock, paper or scissors`, ``).toLowerCase();
+
+	console.log(compChoice, playerChoice);
+
+	if ((playerChoice === `rock` && compChoice === `scissors`) ||
+			(playerChoice === `paper` && compChoice === `rock`) || 
+			(playerChoice === `scissors` && compChoice === `paper`)) {
+
+				console.log(`you win`);
+
+	} else if (playerChoice === compChoice ) {
+		console.log(`it's a draw`);
+	} else {
+		console.log(`you lose`);
+	}
+};
+
+playGame();
