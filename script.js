85412b1c41c32a3645a0ca5778e4d4247f1e9143
Fintan Ryan <fintonius.mcginty@gmail.com>
2022-08-12 17:10:13 +0100

    update script to use buttons for user input and declare winner of 1st to 5

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
-let rockBtn = document.getElementById('rock');
-rockBtn.addEventListener('click', () => console.log('rock'));
-
 function game() {
 
+	let playerChoice = '';
+	let roundResult = document.getElementById('round-result');
+
+	let rockBtn = document.getElementById('rock');
+	rockBtn.addEventListener('click', () => {
+		playerChoice = 'rock';
+		playGame();
+	});
+
+	let paperBtn = document.getElementById('paper');
+	paperBtn.addEventListener('click', () => {
+		playerChoice = 'paper';
+		playGame();
+	});
+
+	let scissorBtn = document.getElementById('scissors');
+	scissorBtn.addEventListener('click', () => {
+		playerChoice = 'scissors';
+		playGame();
+	});
+
+	let winner = document.getElementById('winner');
+
 	let playerScore = 0;
+	let playerPoints = document.getElementById('player-score');
+
 	let compScore = 0;
-	let round = 5;
+	let compPoints = document.getElementById('comp-score');
+	// let round = 5;
 
 	function playGame () {
 		const choices = [`rock`, `paper`, `scissors`];
 		let compChoice = choices[Math.floor(Math.random() * 3)];
-		let playerChoice = prompt(`Player, pick your weapon: rock, paper or scissors`, ``).toLowerCase();
+		// let playerChoice = prompt(`Player, pick your weapon: rock, paper or scissors`, ``).toLowerCase();
+
+		if (playerScore < 5 && compScore < 5) {
 	
 		if ((playerChoice === `rock` && compChoice === `scissors`) ||
 				(playerChoice === `paper` && compChoice === `rock`) || 
 				(playerChoice === `scissors` && compChoice === `paper`)) {
 	
-					console.log(`${playerChoice.toUpperCase()} beats ${compChoice}, you win`);
-					return playerScore += 1;
+					roundResult.textContent = `${playerChoice.toUpperCase()} beats ${compChoice}, you win`;
+					playerPoints.textContent = `${playerScore+=1}`;
+					return playerScore
 					
 		} else if (playerChoice === compChoice ) {
-			console.log(`${playerChoice} and ${compChoice} are the same, it's a draw`);
+			roundResult.textContent = `${playerChoice} and ${compChoice} are the same, it's a draw`;
 		} else {
-			console.log(`${compChoice.toUpperCase()} beats ${playerChoice} - you lose, loser`);
-			return compScore += 1;
+			roundResult.textContent = `${compChoice.toUpperCase()} beats ${playerChoice} - you lose, loser`;
+			compPoints.textContent = `${compScore+=1}`;
+			return compScore;
 		}
+	} else if (playerScore === 5) {
+					winner.textContent = `Your score is ${playerScore} vs. the comp's score of ${compScore}, you win ye lucky prick`;
+				} else {
+					winner.textContent = `The comp has kicked your ass ${compScore} to ${playerScore}! You lose, chump`;
+				};
 	};
+
+	
+	
 
 	//To play up to the round limit
 // 	for (let i = 1; i <= round; i++) { 
